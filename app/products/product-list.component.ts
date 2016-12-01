967284c98a7675c905501e447b76ae70421a6d55
rakshitsaxena <saxena.rakshit@gmail.com>
2016-12-01 07:55:22 +0530

    added star component

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -46,4 +46,8 @@ export class ProductListComponent
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
+    
+    onRatingClicked(message: string): void{
+        this.pageTitle = 'Product List: ' + message;
+    }
 }
